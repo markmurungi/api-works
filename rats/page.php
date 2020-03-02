e78d0c42cf56b204287c88139b5638f54bfc85a0
markmurungi <markmurungi8@gmail.com>
2020-03-02 12:30:18 +0300

    first commit

---
+++ rats/page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>screencaster</title>
+</head>
+<body>
+Firstnumber:<br>
+<input type="interger" name="$this"><br>
+secondnumber:<br>
+<input type="interger" name="$that"><br>
+
+<button>multiply:
+<a href="screencaster.php" >
+	if($this && $that >=0){
+		$numbers=$z+$that;
+		$that=$loop;
+		while (&that>0) {
+			$numbers=$this+$that;
+			$loop--;
+			# code...
+		}
+		echo $numbers;
+	}
+</button>
+
+
+</body>
+</html>
---
+++ rats/screencaster.php
@@ -0,0 +1,22 @@
+
+<?php
+	
+
+function numbers($this, $that){
+	if($this && $that >=0){
+		$numbers=$z;
+		$that=$loop;
+		while (&that>0) {
+			$numbers=$this+$that;
+			$loop--;
+			# code...
+		}
+		echo $numbers;
+	}
+	
+	elseif () {
+		echo "the numbers are not positive numbers";
+	}
+	
+echo "numbers";
+?>
